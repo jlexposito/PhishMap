7c81b7102aad9198ace565c580da3aa87064c462
José Luis Expósito <jlexposito7@gmail.com>
2017-05-25 19:56:45 +0200

    Split code by functions, add comments and change variables naming convention

--- main.r
+++ main.r
@@ -3,15 +3,23 @@ library(rworldmap)
 library(RColorBrewer)
 library(RCurl)
 
+#Download data file
 API_KEY = "ffe481035f0d5033c16498796ae37d19d62261486c93b9b67d84638fe8d85b28"
 source_url = paste("http://data.phishtank.com/data", API_KEY, "online-valid.json.bz2", sep = "/")
-
 download.file("http://data.phishtank.com/data/online-valid.json.bz2",destfile="data.json",method="libcurl")
-
 file = "data.json"
 data <- fromJSON(file)$details
-countries = unlist(sapply(data, function(data) data$country))
+
+#Create Countries data frame
+countries = unlist( sapply(data, function(data) data$country[data$country != ''] ) )
 countries_tables <- as.data.frame(table(countries))
-spdf <- joinCountryData2Map(countries_tables, joinCode="ISO2", nameJoinColumn="countries", verbose = T)
-colorPalette <- RColorBrewer::brewer.pal(n = 7, name = "YlOrRd")
-mapCountryData(spdf, mapTitle = "PhishMap", nameColumnToPlot="Freq", catMethod = "logFixedWidth", colourPalette = colorPalette, addLegend = F, lwd = 1.5)
+countries_tables <- countries_tables[ order(-countries_tables[,2], countries_tables[,1]), ]
+
+#Generate MapData
+map_data <- joinCountryData2Map(countries_tables, joinCode="ISO2", 
+                            nameJoinColumn="countries", verbose = T)
+#Change MapColorPalette
+map_color_palette <- RColorBrewer::brewer.pal(n = 7, name = "YlOrRd")
+map_country_data <- mapCountryData(map_data, mapTitle = "PhishMap", nameColumnToPlot="Freq", 
+               catMethod = "logFixedWidth", colourPalette = map_color_palette, 
+               addLegend = F, lwd = 1.5)
