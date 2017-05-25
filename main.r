0f060e816777b3cc1525cb8e665688616d636549
Carlos Ortigoza Dempster <carlos.otgz@gmail.com>
2017-05-25 20:09:27 +0200

    Removing API key

--- main.r
+++ main.r
@@ -4,8 +4,6 @@ library(RColorBrewer)
 library(RCurl)
 
 #Download data file
-API_KEY = "ffe481035f0d5033c16498796ae37d19d62261486c93b9b67d84638fe8d85b28"
-source_url = paste("http://data.phishtank.com/data", API_KEY, "online-valid.json.bz2", sep = "/")
 download.file("http://data.phishtank.com/data/online-valid.json.bz2",destfile="data.json",method="libcurl")
 file = "data.json"
 data <- fromJSON(file)$details
