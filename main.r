48c80ee63d550f9019d35ca31dd1bc1ae950a2ec
José Luis Expósito <jlexposito7@gmail.com>
2017-05-30 20:29:46 +0200

    Get data as dataFrame and change all references. Filter only USA to ip_tables

--- main.r
+++ main.r
@@ -18,19 +18,16 @@ library(stringr)
 #Download data file
 download.file("http://data.phishtank.com/data/online-valid.json.bz2",destfile="data.json",method="libcurl")
 file = "data.json"
-data <- fromJSON(file)$details
+data.raw <- fromJSON(file)$details
+data.dataFrame <- do.call(rbind.data.frame, data.raw)
+keep <- c("ip_address", "cidr_block", "country")
+data.dataFrame <- data.dataFrame[keep]
 
 #Create Countries data frame
-countries = unlist( sapply(data, function(data) data$country[data$country != ''] ) )
+data.dataFrame <- dplyr::filter(data.dataFrame, country != '')
+countries <- unlist(data.dataFrame$country)
 countries_tables <- as.data.frame(table(countries))
 countries_tables <- countries_tables[ order(-countries_tables[,2], countries_tables[,1]), ]
-
-#Create IP data frame
-IP = unlist( sapply(data, function(data) data$ip_address[data$ip_address != ''] ) )
-ip_tables <- as.data.frame(IP, stringsAsFactors = F)
-
-#Find mot used IP addresses
-ip_tables <- ip_tables[ order(-ip_tables[,2], ip_tables[,1]), ]
 
 #Generate MapData
 map_data <- joinCountryData2Map(countries_tables, joinCode="ISO2", 
@@ -40,6 +37,7 @@ map_color_palette <- RColorBrewer::brewer.pal(n = 7, name = "YlOrRd")
 map_country_data <- mapCountryData(map_data, mapTitle = "PhishMap", nameColumnToPlot="Freq", 
                catMethod = "logFixedWidth", colourPalette = map_color_palette, 
                addLegend = F, lwd = 1, borderCol = 'black')
+
 #Top 10 table
 top_ten_countries <- head(countries_tables, 10)
 
@@ -57,8 +55,22 @@ top_ten_countries_bar_plot + scale_fill_brewer(palette="RdBu",
                                                name = "Country code \n(ISO2) ",
                                                type="seq")
 
-#### Custom functions
+#Create IP data frame
+data.dataFrame <- dplyr::filter(data.dataFrame, ip_address != '' )
+usa_data <- dplyr::filter(data.dataFrame, country == 'US')
 
+ip_tables <- as.data.frame(usa_data$ip, stringsAsFactors = F)
+colnames(ip_tables) <- c("ip")
+
+ip_tables <- as.data.frame(str_match(ip_tables$ip, "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?:[.]|$)){4}"), stringsAsFactors = F)
+colnames(ip_tables) <- c("ip")
+
+ip_tables <- dplyr::filter(ip_tables, !is.na(ip))
+
+#Find mot used IP addresses
+ip_tables <- count(ip_tables,ip, sort = T)
+
+#### Custom functions
 ip2long <- function(ip) {
   # transforma a vector de characters
   ips <- unlist(strsplit(ip, '.', fixed = TRUE))
@@ -76,16 +88,18 @@ find.country <- function(ip) {
 ip2countries.url <-  "http://download.db-ip.com/free/dbip-country-2017-05.csv.gz"
 download.file(url = ip2countries.url, destfile = "./countries.csv")
 ip2country <- read.csv("./countries.csv", header = F, stringsAsFactors = F)
+
 names(ip2country) <- c("block_start", "block_end", "country")
-ip_tables$IP <- as.list(str_match(ip_tables$IP, "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?:[.]|$)){4}"))
-ip_tables <- dplyr::filter(ip_tables, !is.na(IP))
+ip2country <- dplyr::filter(ip2country, country == "US")
 
 #### extract ip's & compute numeric equivalent
-ip_tables$ip_long <- sapply(X = ip_tables$IP, ip2long)
+ip_tables$ip_long <- sapply(X = ip_tables$ip, ip2long)
 
 #### compute numeric equivalent for ip blocks
+#<cache> true
 ip2country$block_start_long <- sapply(X = ip2country$block_start, FUN = ip2long)
 ip2country$block_end_long   <- sapply(X = ip2country$block_end, FUN = ip2long)
+#</cache> 
 
 ip2country <- dplyr::filter(ip2country, !is.na(block_start_long))
 ip2country <- dplyr::filter(ip2country, !is.na(block_end_long))
